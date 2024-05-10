a00d5f00ead698401ec56b0f966aef07d4264539
Matt <mattbarich1@gmail.com>
2024-05-09 21:14:06 -0600

    Added Skills and Contact. Edited README, and To-do.

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,9 +1,56 @@
-import React from "react";
+import React from 'react';
 
-function Skills() {
-    return(
-        <div>Skills</div>
-    )
-}
-
+const Skills = () => {
+  return (
+    <div name='skills' className='w-full h-screen bg-[#0a192f] text-gray-300'>
+      {/* Container */}
+      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+          <div className=' w-full flex justify-center items-center flex-col mb-7'>
+              <p className='text-4xl font-bold inline border-b-4 border-cyan-500 text-center '>Skills</p>
+              <p className='py-4 text-2xl'>I enjoy learning new technologies to better my foundational coding skills. I also 
+              enjoy competing in CTF's and challenges on TryHackMe, HacktheBox to improve my cybersecurity skills and techniques.
+               Here's a list of technologies I've worked with</p>
+          </div> 
+          <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <p className='my-4'>Java</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <p className='my-4'>C/C++</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <p className='my-4'>Python</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <p className='my-4'>Linux OS</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <p className='my-4'>XenServer/XCP-ng</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <p className='my-4'>Vulnerability Management</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <p className='my-4'>HTML</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <p className='my-4'>CSS</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <p className='my-4'>JAVASCRIPT</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <p className='my-4'>REACT</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <p className='my-4'>Github</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <p className='my-4'>Last skill Here</p>
+              </div>
+          </div>
+      </div>
+    </div>
+  );
+};
 export default Skills
